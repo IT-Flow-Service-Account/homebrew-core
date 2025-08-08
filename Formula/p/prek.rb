7d129314db4be3daf991e429d2924c9e2500a715
Branch Vincent <branchevincent@gmail.com>
2025-08-08 08:56:40 -0700

    prek: renamed from prefligit

--- Formula/p/prek.rb
+++ Formula/p/prek.rb
@@ -1,10 +1,10 @@
-class Prefligit < Formula
+class Prek < Formula
   desc "Pre-commit re-implemented in Rust"
-  homepage "https://github.com/j178/prefligit"
-  url "https://github.com/j178/prefligit/archive/refs/tags/v0.0.21.tar.gz"
+  homepage "https://github.com/j178/prek"
+  url "https://github.com/j178/prek/archive/refs/tags/v0.0.21.tar.gz"
   sha256 "e4122150b44d39f7693ca8c2676fe07fa1f2607cf42302c8b8447619aafa311e"
   license "MIT"
-  head "https://github.com/j178/prefligit.git", branch: "master"
+  head "https://github.com/j178/prek.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "47ac7a04e1feef9d204735c6f904b628361b9ab8884c0216a8c1eb00d7145200"
@@ -20,13 +20,13 @@ class Prefligit < Formula
 
   def install
     system "cargo", "install", *std_cargo_args
-    generate_completions_from_executable(bin/"prefligit", "generate-shell-completion")
+    generate_completions_from_executable(bin/"prek", "generate-shell-completion")
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/prefligit --version")
+    assert_match version.to_s, shell_output("#{bin}/prek --version")
 
-    output = shell_output("#{bin}/prefligit sample-config")
+    output = shell_output("#{bin}/prek sample-config")
     assert_match "See https://pre-commit.com for more information", output
   end
 end
